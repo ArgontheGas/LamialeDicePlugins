96d71da5f35c226a6feef2c1660d0af18227dc1f
ArgontheGas <argontheunnoblegas@gmail.com>
2021-06-20 01:34:11 +0800

    Update color.lua

--- color.lua
+++ color.lua
@@ -2,7 +2,14 @@ command = {}
 
 
 
+function Info(msg)
+    do
+        return "抽取颜色：.color draw\n生成hex色码图片：.hex 色码\n生成hsl色码图片：.hsl 色码\n生成rgb色码图片：.rgb 色码"
+    end
+end
 
 command["(\\.|。)color draw"]= "random"
-command["(\\.|。)color\\s(\\d+)"] = "selected"
-command["(\\.|。)color"] = "info"
+command["(\\.|。)hsl\\s(\\d+)"] = "hslpic"
+command["(\\.|。)rgb\\s(\\d+)"] = "rgbpic"
+command["(\\.|。)hex\\s(\\[0-9a-fA-F]+)"] = "hexpic"
+command["(\\.|。)color"] = "Info"
